84005eee7b30f585a994d53237ed9b37826ed0f5
vaidehi <vaidehi117v@gmail.com>
2023-09-25 13:42:58 -0400

    setup tailwind

--- src/App.js
+++ src/App.js
@@ -1,5 +1,9 @@
+import { useState } from "react";
+import useMediaQuery from "./hooks/useMediaQuery";
 
 function App() {
+  const [selectedPage, setSelectedPage] = useState('home');
+  const isAboveMediumScreen = useMediaQuery("min-width: 1060px");
   return (
     <div className="App">
 
